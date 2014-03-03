655619e74bfdd2eabb47b1e13186091950d80eef
artourkin <artur.kulmukhametov@gmail.com>
2014-03-03 18:49:28 +0100

    clean up

--- correlation tests.R
+++ correlation tests.R
@@ -1,28 +1,23 @@
-rawdata1 <-read.csv("/home/artur/rnd/git/perceptualqa/ssim_ADC.csv")
-rawdata1 <- ifelse(rawdata1 == "Image size does not match", NA, x)
-#rawdata1[rawdata1 == "Image size does not match"]<-NA
-rawdata2 <-read.csv("/home/artur/rnd/git/perceptualqa/ssim_CO.csv")
-rawdata3 <-read.csv("/home/artur/rnd/git/perceptualqa/ssim_DxO.csv")
-rawdata<-rbind(rawdata1,rawdata2,rawdata3)
-attach (rawdata)
-rawdata$r_aver<-(r1_brightness+r1_clipping+r1_hue+r2_brightness+r2_clipping+r2_hue)/18
-rawdata$ssim_aver<-(ssimSimple+ssimSimpleHue+ssimSimpleSaturation)/3
-rawdata$ae_aver<-(aeRelative+paeRelative+maeRelative+mseRelative)/4
-attach (rawdata)
-pairs(~r1_clipping+r1_brightness+r1_hue+r2_clipping+r2_brightness+r2_hue,data=rawdata3, 
-      main="Simple Scatterplot Matrix")
-#rawdata<-cbind(r1_clipping,
-#               r1_brightness,
-#               r1_hue,
-#               r2_clipping,
-#               r2_brightness,
-#               r2_hue
-#               ,ssimSimple,ssimSimpleHue,ssimSimpleSaturation
-#               )  
-library(corrgram)
-corrgram(rawdata, order=TRUE, lower.panel=panel.shade,
-         upper.panel=panel.pie, text.panel=panel.txt,
-         main="Perceptual QA")
-
-
+rawdata_ADC <-read.csv("/home/artur/rnd/git/perceptualqa/rawdata_ADC.csv")
+rawdata_CO <-read.csv("/home/artur/rnd/git/perceptualqa/rawdata_CO.csv")
+rawdata_DxO <-read.csv("/home/artur/rnd/git/perceptualqa/rawdata_DxO.csv")
+rawdata_ADC[rawdata_ADC=="Image size does not match"] <- NA
+rawdata_CO[rawdata_CO=="Image size does not match"] <- NA
+rawdata_DxO[rawdata_DxO=="Image size does not match"] <- NA
+#rawdata<-cbind(rawdata1$r1_clipping,rawdata1$r1_brightness,rawdata1$r1_hue,rawdata1$ssimSimple,rawdata1$ssimSimpleHue,rawdata1$ssimSimpleSaturation)
 #mt<-mtcars
+
+x <- rawdata_DxO[3:8]
+y <- rawdata_DxO[9:15]
+xy <- cbind(x,y)
+
+#stripchart(xy, method = "jitter")
+#xy <- cbind(x,y)
+#print(cor(x, y))
+
+#library(polycor)
+#hetcor(rawdata3) 
+
+library(corrgram)
+corrgram(rawdata_DxO, order=TRUE, lower.panel=panel.shade,
+         upper.panel=panel.pie, text.panel=panel.txt, main="Correlation between metrics in DxO experiment")
